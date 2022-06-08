72c7dcf73d207b0870f6816a24a9670bcc7a4512
Will <willjcGA@outlook.com>
2022-06-08 22:20:26 +0800

    added class structure

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
+// A class to encapsulate logic for displaying books on the display book page
 class Book {
     //
 }
 
-// A class to encapsulate all the logic surrounding a general book search
+// A class to encapsulate all the logic surrounding a book search
 class Search {
     static apiEndpoint = "http://openlibrary.org/search.json";
     constructor() {
         this.generalSearchString = "";
     }
-    search() {
-        // Async search function goes here.
+    // Method to send GET request to OpenLibrary and display response in DOM
+    async displaySearch(url) {
+        const response = await fetch(url);
+        const data = await response.json();
+        // Deal with the search request here
+        // data.docs.forEach(element => {
+        // const div = addSearchItem(element.title);
+        // const bodyElement = document.querySelector("body");
+        // bodyElement.append(div);
     }
     setSearchString() {
         // Method to set the search string
     }
-    // Method to create the html 
+    // Method to create a div element with inner list elements to display the relevant data
     createSearchResultDiv() {
         const divElement = document.createElement("div");
         divElement.innerHTML = `
@@ -25,17 +33,9 @@ class Search {
         return divElement;
     }
     getFormData() {
-        //
+        // Method that fetches the search string from the input field
     }
-}
-async function search(searchString) {
-    const url = `http://openlibrary.org/search.json${searchString}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-    data.docs.forEach(element => {
-        const div = addSearchItem(element.title);
-        const bodyElement = document.querySelector("body");
-        bodyElement.append(div);
-    });
+    submitSearch() {
+        // Function to run with the button click event listener
+    }
 }
