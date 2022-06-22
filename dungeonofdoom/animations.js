ea45a5b00cc5946899b90f0332d0ff81255c82c7
Will <willjcGA@outlook.com>
2022-06-22 22:31:39 +0800

    updating animations

--- dungeonofdoom/animations.js
+++ dungeonofdoom/animations.js
@@ -100,20 +100,6 @@ function updateText(targetElementAsString, text) {
         })
 }
 
-// Rubber band animation
-function rubberBand(targetElement) {
-    return new Promise(resolve => {
-        targetElement.classList.add("rubberBand");
-        console.log("rubberBand animation waiting to complete")
-        targetElement.onanimationend = () => {
-            targetElement.classList.remove("rubberBand");
-            console.log("rubberBand animation complete")
-            resolve();
-        }
-    
-    })
-}
-
 // Stab attack animation
 async function stabAttack(attacker, target, isHit, startText, successFailText) {
     return new Promise(async resolve => {
@@ -145,42 +131,3 @@ async function stabAttack(attacker, target, isHit, startText, successFailText) {
         }
     })
 }
-
-// Hit animation
-function flash(targetElement) {
-    return new Promise(resolve => {
-        targetElement.classList.add("flash");
-        console.log("flash animation waiting to complete")
-        targetElement.onanimationend = () => {
-            targetElement.classList.remove("flash");
-            console.log("flash animation complete")
-            resolve();
-        }
-    })    
-}
-// Death animation
-function death(targetElement) {
-    return new Promise(resolve => {
-        targetElement.classList.add("dead");
-        console.log("death animation waiting to complete")
-        setTimeout(() => {
-            targetElement.classList.add("hidden");
-            console.log("death animation complete")  
-        }, 1000)
-        resolve();
-    })
-}
-// Enemy attack
-function bounceOutLeft(targetElement) {
-    return new Promise(resolve => {
-        targetElement.classList.add("bounceOutLeft");
-        console.log("bounceOutLeft animation waiting to complete")
-        targetElement.onanimationend = () => {
-            targetElement.classList.remove("bounceOutLeft");
-            console.log("bounceOutLeft animation complete")
-            resolve();
-        }
-    })
-}
-
-
