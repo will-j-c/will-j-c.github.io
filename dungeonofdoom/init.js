2848a1f0d1685b01cf1399442eb34f8c14f6a5e9
Will <willjcGA@outlook.com>
2022-06-23 20:56:00 +0800

    added battle log

--- dungeonofdoom/init.js
+++ dungeonofdoom/init.js
@@ -1,16 +1,16 @@
 //Initiate game
 let game = new Game(levelParams);
-// game.init();
-const level = new Level({
-    player: new Player(), 
-    levelParamObject: levelParams[0], 
-    game: game, 
-    onEndLevel: event => {
-            resolve();
-    }
-})
+game.init();
+// const level = new Level({
+//     player: new Player(), 
+//     levelParamObject: levelParams[0], 
+//     game: game, 
+//     onEndLevel: event => {
+//             resolve();
+//     }
+// })
 
-level.initiateBattle();
+// level.initiateBattle();
 // Init a battle
 // const battle = new Battle(new Player(), new Skeleton(), new Skeleton(), new Skeleton(), onend);
 // function onend() {
--- dungeonofdoom/levels/levelParams.js
+++ dungeonofdoom/levels/levelParams.js
@@ -23,25 +23,25 @@ const levelParams = [
             "As you near the gate, there is a pulsing of energy, dark and sickly, it makes you want to vomit. As you recover and look up, you see three dead men things approach. Guardians of the castle with rusty weapons in hand….."
         ],
         tile: "./assets/tiles/grass.jpg"
-    },
-    {
-        chapterTitle: "Chapter 2: Hall of Horror",
-        battleLineup: [new Ghoul, new Ghoul, new Ghoul],
-        text: [
-            "You arrive at the castle after three days forced march. The exterior is quiet and no light shows from the windows.",
-            "You approach wearily, knowing you are being watched. Crows flock and caw overhead, indicating the presence of carrion.",
-            "As you near the gate, there is a pulsing of energy, dark and sickly, it makes you want to vomit. As you recover and look up, you see three dead men things approach. Guardians of the castle with rusty weapons in hand….."
-        ],
-        tile: ""
-    },
-    {
-        chapterTitle: "Chapter 3: Hall of Horror",
-        battleLineup: [new Skeleton, new Skeleton, new Skeleton],
-        text: [
-            "You arrive at the castle after three days forced march. The exterior is quiet and no light shows from the windows.",
-            "You approach wearily, knowing you are being watched. Crows flock and caw overhead, indicating the presence of carrion.",
-            "As you near the gate, there is a pulsing of energy, dark and sickly, it makes you want to vomit. As you recover and look up, you see three dead men things approach. Guardians of the castle with rusty weapons in hand….."
-        ],
-        tile: ""
-    }
+    // },
+    // {
+    //     chapterTitle: "Chapter 2: Hall of Horror",
+    //     battleLineup: [new Ghoul, new Ghoul, new Ghoul],
+    //     text: [
+    //         "You arrive at the castle after three days forced march. The exterior is quiet and no light shows from the windows.",
+    //         "You approach wearily, knowing you are being watched. Crows flock and caw overhead, indicating the presence of carrion.",
+    //         "As you near the gate, there is a pulsing of energy, dark and sickly, it makes you want to vomit. As you recover and look up, you see three dead men things approach. Guardians of the castle with rusty weapons in hand….."
+    //     ],
+    //     tile: ""
+    // },
+    // {
+    //     chapterTitle: "Chapter 3: Hall of Horror",
+    //     battleLineup: [new Skeleton, new Skeleton, new Skeleton],
+    //     text: [
+    //         "You arrive at the castle after three days forced march. The exterior is quiet and no light shows from the windows.",
+    //         "You approach wearily, knowing you are being watched. Crows flock and caw overhead, indicating the presence of carrion.",
+    //         "As you near the gate, there is a pulsing of energy, dark and sickly, it makes you want to vomit. As you recover and look up, you see three dead men things approach. Guardians of the castle with rusty weapons in hand….."
+    //     ],
+    //     tile: ""
+    // }
 ]
