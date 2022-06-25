f44507f2d4448f9f211da32c93a212649ab11245
Will <willjcGA@outlook.com>
2022-06-25 22:25:36 +0800

    added bounce animations

--- dungeonofdoom/init.js
+++ dungeonofdoom/init.js
@@ -1,12 +1,12 @@
 //Initiate game
 let game = new Game(levelParams);
-// game.init();
-const level = new Level({
-    player: new Player(), 
-    levelParamObject: levelParams[0], 
-    game: game, 
-    onEndLevel: event => {
-            resolve();
-    }
-})
-level.initiateBattle();
+game.init();
+// const level = new Level({
+//     player: new Player(), 
+//     levelParamObject: levelParams[0], 
+//     game: game, 
+//     onEndLevel: event => {
+//             resolve();
+//     }
+// })
+// level.initiateBattle();
